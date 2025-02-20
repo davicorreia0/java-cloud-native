c74b7f17e3a9ffa6dec2cdef701173284681271b
Davi Correia <davicorreia0@hotmail.com>
2025-02-20 15:59:20 -0300

    Anatomia das classes

---
+++ java-anatomia-de-classes/src/BoletimEstudantil.java
@@ -0,0 +1,11 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        double mediaFinal = 5.9;
+            if(mediaFinal<6)
+                System.out.println("BURRO DEMAIS");
+            else if (mediaFinal==6)
+                System.out.println("No limite, mas passou");
+            else 
+                System.out.println("Aprovado");
+        }
+}
--- java-anatomia-de-classes/src/MinhaClasse.java
+++ java-anatomia-de-classes/src/MinhaClasse.java
@@ -1,7 +1,14 @@
 public class MinhaClasse {
     public static void main (String [] args){
-        System.out.println("HEHEHEHEH");
+        String primeiroNome = "Davi, ";
+        String segundoNome = "O gostosão!";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
 
     }
 
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do metodo: " + primeiroNome.concat(" ").concat(segundoNome);
+    }
 }
